1d6b0987192815fc0bf1335d2e44abe02c7792ce
mamyR5 <143075478+mamyR5@users.noreply.github.com>
2026-03-29 13:09:47 +0300

    Update Docker

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,18 @@
-# --- ÉTAPE 1 : Compilation avec Maven et Java 21 ---
 FROM maven:3.9.6-eclipse-temurin-21 AS build
 WORKDIR /app
 
-# On copie le fichier de configuration Maven
 COPY pom.xml .
-# On copie tout le dossier src (ton code et tes JSP)
 COPY src ./src
 
-# On lance la compilation
 RUN mvn clean package
 
-# --- ÉTAPE 2 : Serveur Tomcat 10 ---
 FROM tomcat:10.1-jdk21
 
-# Nettoyage des applications par défaut de Tomcat
 RUN rm -rf /usr/local/tomcat/webapps/*
 
-# On copie le driver Postgres depuis ton dossier lib local
 COPY lib/postgresql-42.7.9.jar /usr/local/tomcat/lib/
 
-# On récupère le fichier .war généré à l'étape 1 pour le mettre dans Tomcat
+# On récupère le fichier .war généré pour le mettre dans Tomcat
 # Maven nomme le fichier "ROOT.war" car nous l'avons précisé dans le pom.xml
 COPY --from=build /app/target/ROOT.war /usr/local/tomcat/webapps/
 
--- src/main/java/webdesign/controller/TestServlet.java
+++ src/main/java/webdesign/controller/TestServlet.java
@@ -1,11 +1,11 @@
 package webdesign.controller;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class TestServlet extends HttpServlet {
 
@@ -13,14 +13,10 @@ public class TestServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
         
-        // 1. Récupérer le "slug" de l'URL (ex: /guerre-iran)
         String pathInfo = request.getPathInfo(); 
         
-        // 2. Transmettre l'info à la JSP pour vérification
         request.setAttribute("slugDetecte", pathInfo);
 
-        // 3. Forward vers la JSP de test dans le dossier Front-Office 
-        // Le chemin part de la racine de 'webapp'
         request.getRequestDispatcher("/WEB-INF/views/front-office/test.jsp")
                .forward(request, response);
     }
